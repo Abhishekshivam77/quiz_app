a0a9de3d7449a3e786db0e1413bfa6deb8cf0775
Abhishekshivam77 <shivamkmr37@gmail.com>
2023-12-20 00:19:43 +0530

    model created

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-const express = require("express");
-const { connection } = require("./config/db");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 
-
-
-
+const connection = require('./config/config')
+const {userRouter} = require('./routes/user.route')
+const {quizRouter} = require('./routes/quiz.route');
 
 const app = express();
+const port = process.env.port;
 
-//middleware
+app.use(express.json())
+app.use("/user",userRouter);
+app.use("/quiz",quizRouter);
 
-app.use(express.json());
-
-
-
-
-
-//routes
-app.get("/",(req,res)=>{
-    res.status(200).send("Welcome to the QUIZ APP ☻ ")
+app.get('/', (req, res) =>{
+    res.send("Welcome to the QUIZ APP ☻ ☻..")
 })
 
-
-
-
-
-app.listen(process.env.PORT,async()=>{
-    try {
-        await connection;
-        console.log(`Connected to db!`)
-    } catch (error) {
-        console.log(`Unable to connect db!`);
-        console.log(error.message)
+app.listen(port, async(req,res)=>{
+    try{
+        await connection 
+        console.log('database is connected');
+    } catch(err){
+        console.log(err.message);
+        console.log("server is not running..")
     }
-    console.log(`App is runnig on the port ${process.env.PORT}!`)
+    console.log(`server is running on port ${process.env.port}`);
 })
